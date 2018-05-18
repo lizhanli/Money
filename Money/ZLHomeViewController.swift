4b6b258a222cddcbc51c5cb7230d042c7d1e3ab6
李占理 <zhanli.li@pactera.com>
2018-05-18 11:02:25 +0800

    no message

--- Money/ZLHomeViewController.swift
+++ Money/ZLHomeViewController.swift
@@ -12,6 +12,7 @@ class ZLHomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        logInfo("测试")
     }
 
     override func didReceiveMemoryWarning() {
